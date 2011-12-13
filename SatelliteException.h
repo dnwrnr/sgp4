4f249346b1fcae95dca83d3cc314e3a28b609fdb
Daniel Warner <contact@danrw.com>
2011-12-13 23:05:57 +0000

    Updated SatelliteException

--- SatelliteException.h
+++ SatelliteException.h
@@ -4,17 +4,20 @@
 #include <exception>
 #include <iostream>
 
-class SatelliteException : public std::exception {
+class SatelliteException : public std::exception
+{
 public:
-
     SatelliteException(const char* message)
-    : message_(message) {
+        : message_(message)
+    {
     }
 
-    virtual ~SatelliteException(void) throw () {
+    virtual ~SatelliteException(void) throw ()
+    {
     }
 
-    virtual const char* what() const throw () {
+    virtual const char* what() const throw ()
+    {
         return message_.c_str();
     }
 
